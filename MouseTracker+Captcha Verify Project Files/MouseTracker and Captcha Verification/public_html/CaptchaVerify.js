907ff9bb9d84720ae9f51c2cc2d97f2746250f9a
ncampbell282 <82889469+ncampbell282@users.noreply.github.com>
2021-04-30 09:05:33 -0500

    Add base functions to retrieve necessary data from webpage

--- MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/CaptchaVerify.js
+++ MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/CaptchaVerify.js
@@ -2,8 +2,28 @@
  * Nolan Campbell
  * This script will verify that mouse movement is detected, and allow the user to attempt to login to Tyler SIS
  */
+attemptCount = 0;
 
-
+//Get if mouse movement is detected or not
 function verify() {
     document.getElementById("demo").innerHTML = "Verified";
+}
+
+//Get entered username
+function getUsername() {
+    return document.getElementById("username").value;
+}
+
+//get entered password
+function getPassword() {
+    return document.getElementById("password").value;
+}
+
+//Attempt to login
+function attemptLogin() {
+    username = getUsername();
+    password = getPassword();
+
+    //Send password to database
+    //If database returns false match, increment attempt count by one, when attempt count equals 5, start running verify method
 }
