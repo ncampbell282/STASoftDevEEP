90911fab436772ad0a9a29295780bef417f4ecfc
ncampbell282 <82889469+ncampbell282@users.noreply.github.com>
2021-04-30 09:52:42 -0500

    Begin scripting system to receive if login attempt was successful from a database

--- MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/CaptchaVerify.js
+++ MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/CaptchaVerify.js
@@ -21,9 +21,27 @@ function getPassword() {
 
 //Attempt to login
 function attemptLogin() {
-    username = getUsername();
-    password = getPassword();
 
-    //Send password to database
-    //If database returns false match, increment attempt count by one, when attempt count equals 5, start running verify method
+    //Get username and password
+    var username = getUsername();
+    var password = getPassword();
+
+    //Set the data to send in request to PHP file
+    var sendData = JSON.stringify({
+        name: username,
+        pass: password
+    });
+
+    //Send password to PHP file using AJAX/xmlhttp to interface with database, and return if login successful
+    //If PHP file returns false match, increment attempt count by one, when attempt count equals 5, start running verify method
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+
+    xmlhttp.open("POST", "verifyLogin.php?q=", true);
+    xmlhttp.send(sendData);
 }
---
+++ MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/verifylogin.php
@@ -0,0 +1,15 @@
+<?php
+$mysqli = new mysqli("jdbc:mysql://localhost:3306", "root", "password", "sisusers");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "SELECT username, password, address, WHERE username = name AND password = pass";
+
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($username, $password);
+$stmt->fetch();
+$stmt->close();
