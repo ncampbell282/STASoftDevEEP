6403cccefc0db26f68243daba5decca678b13545
NC2357 <ncampbell282@lsr7.net>
2021-05-03 12:58:26 -0500

    Update verifylogin.php

--- MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/verifylogin.php
+++ MouseTracker+Captcha Verify Project Files/MouseTracker and Captcha Verification/public_html/verifylogin.php
@@ -1,16 +1,24 @@
 <?php
-$mysqli = new mysqli("jdbc:mysql://localhost:3306", "root", "password", "sisusers");
+//Connect to database with the following parameters
+$mysqli = new mysqli("localhost:3306", "root", "password","sisusers");
+//Check connection
 if($mysqli->connect_error) {
   exit('Could not connect');
 }
 
-$sql = "SELECT username, password, address, WHERE username = name AND password = pass";
+//Create query to run on the database
+$sql = "SELECT username, password FROM sisusrnameandpassword WHERE username = 'nolan.campbell461' AND password = 'best6713'";
 
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param("s", $_GET['q']);
-$stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($username, $password);
-$stmt->fetch();
-$stmt->close();
+//Send query to database, and store result in $result
+$result = $mysqli->query($sql);
+
+//Check if result has results
+if ($result->num_rows > 0) {
+  // output data of each row in results
+  while($row = $result->fetch_assoc()) {
+    echo "username: " . $row["username"]. " - Password: " . $row["password"]. " " . "<br>";
+  }
+} else {
+  echo "Invalid login";
+}
 ?>
